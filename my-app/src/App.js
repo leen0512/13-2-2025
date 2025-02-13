eca889da961f077f05b675b1a896e2a919e9763f
leen <leen051206@gmail.com>
2025-02-13 10:57:06 +0200

    first question in state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-import Message from './components/Message';
+import Message from './components/props/Message';
+import NumberDisplay from './components/props/NumberDisplay';
+import TextChanger from './components/state/TextChanger';
 
 function App() {
+  //props
   const [message, setMessage] = useState("message...");
   const [number1, setNumber1] = useState(1);
   const [number2, setNumber2] = useState(2);
   const [number3, setNumber3] = useState(3);
+
+  //state
+  const [text, setText] = useState("welcome");
+
   return (
     <div className="App">
-     
+     {/* props */}
       <Message message={message}></Message>
-
+      <NumberDisplay number1={number1} number2={number2} number3={number3}></NumberDisplay>
+    
+      {/* state */}
+      <TextChanger text={text}></TextChanger>
+      <button onClick={() => setText("Hello World")}>Click Here to Change Text</button>
     </div>
   );
 }
---
+++ my-app/src/components/state/TextChanger.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function TextChanger(props){
+    return <div>
+        <p>{props.text}</p>
+    </div>
+}
+
+export default TextChanger;
